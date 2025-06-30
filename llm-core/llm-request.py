51272704efde9a949a73303eb89645bc09a171bd
simonzyWu <simonwuzy412@gmail.com>
2025-06-30 21:16:17 +0800

    feat: 启动后台项目

---
+++ llm-core/llm-request.py
@@ -0,0 +1,15 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-79d7980164854184a37a8f6e2be6a3d6", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
